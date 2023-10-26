73ff480fe05422094629985fd717d8c4f7094437
shady-mungai <shadrackmungai10@gmail.com>
2023-10-26 08:17:37 +0300

    Added content to Home Page

---
+++ src/app/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>DELL-LIGHT</h1>
+        <p>ICONS</p>
+
+        <h1>COMPANY</h1>
+        <h2>WHO WE ARE</h2>
+        <p>
+        "Since our establishment in 2008, Dell-light Pharmaceuticals has been dedicated to making a positive impact on healthcare.
+Our commitment to excellence in medical dispensing has consistently brought smiles to faces and improved the well-being of individuals.
+
+At Dell-light Pharmaceuticals, our mission is clear: we aim to make essential medical drugs and equipment easily accessible to everyone.
+ We believe that quality healthcare should be within reach for all individuals, and we're taking concrete steps to make this a reality. 
+        </p>
+        <img src=''>
+        </img>
+    </div>
+  )
+}
+
+export default Home
