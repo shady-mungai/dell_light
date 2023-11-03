a81e4cf4a84087ded948a647103456f807b5d3a6
Jeff-Maina <devyoungblud@gmail.com>
2023-11-03 12:49:31 +0300

    Optimized hero section for large screens

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,35 @@
+const Navbar = () => {
+  const pageLinks = [
+    {
+      sectionName: "Faqs",
+      sectionLink: "#faqs",
+    },
+    {
+      sectionName: "Services",
+      sectionLink: "#services",
+    },
+    {
+      sectionName: "Contacts",
+      sectionLink: "#contact",
+    },
+    {
+      sectionName: "Our Story",
+      sectionLink: "#story",
+    },
+  ];
+  return (
+    <nav className="w-full h-[4.4rem] lg:h-[6rem] flex items-center px-4 lg:px-20 m-auto justify-between">
+      <p className="font-black lg:text-2xl">
+        <span className="text-yellow-500">Dell</span> Light
+      </p>
+
+      <div className="text-yellow-400 hidden lg:flex gap-10">
+        {pageLinks.map(({ sectionName, sectionLink }) => (
+          <a className="text-xl font-bold" href={sectionLink}>{sectionName}</a>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
